4edb1e46bb23c0fb02ddafe40ceb91a2ebcff649
vamshichi <chvamshi03@gmail.com>
2026-03-12 23:15:39 +0530

    changed shop fetch api

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'your-domain.com',
-    },
-  ],
-}
+   images: {
+    domains: ["res.cloudinary.com"],
+  },
 };
 
 export default nextConfig;
